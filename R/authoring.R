a99f879a86ec77c27dfe413d9669b713b383a630
Choonghyun Ryu <choonghyun.ryu@gmail.com>
2025-02-23 22:06:25 +0900

    채팅 결과의 요약 및 편집 기능 추가 (#1)

--- R/authoring.R
+++ R/authoring.R
@@ -5,3 +5,29 @@ chat2markdown <- function(x) {
     trim_before_string("Assistant\n\n") |>
     cat()
 }
+
+#' @export
+#' @import ellmer
+edit_openai <- function(x, type = c("summary", "edit")) {
+  type <- match.arg(type)
+
+  type_summary <- type_object(
+    "요약",
+    summary = type_string("내용을 요약해 줘. 최대 4~5 paragraphs")
+  )
+
+  type_edit <- type_object(
+    "편집",
+    edit    = type_string("문장이 지나치게 길거나 복잡한 경우 가독성을 높이도록 개선해 줘.")
+  )
+
+  .chat <- chat_openai()
+
+  if (type == "summary") {
+    data <- .chat$extract_data(contents_markdown(x), type = type_summary)
+    return(data$summary)
+  } else if (type == "edit") {
+    data <- .chat$extract_data(contents_markdown(x), type = type_edit)
+    return(data$edit)
+  }
+}
